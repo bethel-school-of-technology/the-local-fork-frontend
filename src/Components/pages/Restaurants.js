4f32cb8237f79beddc196920c355f08cb6d7b1fa
Patrick Hern <mr.hern@Patricks-iMac.lan>
2021-11-09 13:11:26 -0700

    Added to the app.js Params to the restaurants. Have the abilty to click to a rest and to edit a review

--- src/Components/pages/Restaurants.js
+++ src/Components/pages/Restaurants.js
@@ -1,5 +1,6 @@
-// import axios from "axios";
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 // import Newres from "./Newres";
@@ -8,56 +9,72 @@ import Button from 'react-bootstrap/Button'
 
 
 function Restaurants() {
-  const [reviewData, setreviewData] = useState([]);
+  // const [reviewData, setreviewData] = useState([]);
   const [data, setData] = useState([]);
-  // console.log(data)
+  console.log(data)
+  const { restaurantName } = useParams()
+  
 
   useEffect(() => {
+    console.log(restaurantName);
+    axios.get(`http://localhost:5000/restaurant/${restaurantName}`).then((res) => {
+      // console.log(res);
+      setData(res.data.data);
+    
+    });
+  }, [restaurantName]);
 
-const resData = JSON.parse(localStorage.getItem("restourantData"));
-console.log(resData.restaurants)
-setreviewData(resData.restaurants)
-setData(resData.restaurants)
-  }, []);
+// const resData = JSON.parse(localStorage.getItem("restourantData"));
+// console.log(resData.restaurants)
+// // setreviewData(resData.restaurants)
+// setData(resData.restaurants)
+//   }, []);
 
   
 
 //   if (!data) return null;
  
-  const reView = reviewData.map((rest, id) => (
-    <ul key={id}>
+  // const currentRestaurants = data.map((rest, id) =>  (
+  //   <ul key={id}>
     
-     Review: "{rest.review}"
-    </ul>
-  ));
+  //     "{rest.name}"
+  //     {rest.hours}
+  //     {rest.location}
+  //     {rest.rating}
+  //   </ul>
+  // ));
 
-//   const currentReviews = post[0].reviews.map((rest, id ) => (
-//     <li key={id}>
-//           <h1>{rest.name}</h1>
-//     <h1>{rest.username}</h1>
-//       <h1>{rest.review}</h1>
+  // const currentReviews = data[0].reviews.map((rest, id ) => (
+  //   <li key={id}>
+  //         <h1>{rest.name}</h1>
+  //   <h1>{rest.username}</h1>
+  //     <h1>{rest.review}</h1>
       
-//     </li>
-    
+  //   </li>
+  // ));
 
-//   ));
-//   const currentRe = data[0].reviews.map((data, id ) => (
-//     <li key={id}>
+  // const currentRe = data[0].reviews.map((data, id ) => (
+  //   <li key={id}>
 
-//       <h3>{data.review}</h3>
+  //     <h3>{data.review}</h3>
       
-//     </li>
+  //   </li>
     
 
-//   ));
+  // ));
   
 
   return (
+
+    <div > 
+  
 <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="https://nightlife.ng/wp-content/uploads/2020/08/9540978_bardanfo_jpeg273c8b91416e05bf0daee14b46a8c03e.jpg" style={{ width: "18rem"}} />
   <Card.Body>
-    <Card.Title><h1>{data.name}</h1></Card.Title>
+    <Card.Title><h1></h1></Card.Title>
     <Card.Text>
+      
+      {data.name}
     Hours {data.hours} a day
     </Card.Text>
     <Card.Text>
@@ -66,13 +83,13 @@ setData(resData.restaurants)
     <Card.Text>
    Rating {data.rating} stars
     </Card.Text>
-    {reView}
-    {/* {{Newres}} */}
+    
+    {/* {{Newres}}  */}
     <Button type ="button"   variant="primary">Add a review?</Button>
   </Card.Body>
   
 </Card>
- 
+</div>
   );
 }
 export default Restaurants;
